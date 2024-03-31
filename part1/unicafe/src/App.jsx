c616db1cd5bbc8b6caf703de41038f112c6fe077
Pablo Benito <pablobenitomar@gmail.com>
2024-03-31 22:28:43 +0200

    Exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
 
+// a proper place to define a component
+const Statistics = ({good,neutral,bad}) => {
+  //const {good,neutral,bad} = props
+  return (
+    <>
+    <h1>Statistics</h1>
+    <div>good: {good}</div>
+    <div>neutral: {neutral}</div>
+    <div>bad: {bad}</div>
+    <div>all: {good+neutral+bad}</div>
+    <div>average: {(good-bad)/(good+neutral+bad)}</div>
+    <div>positive: {(good)/(good+neutral+bad)*100} %</div>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
-  const getAll=()=> good+neutral+bad
-  const getAverage=()=>(good-bad)/getAll()
-  const getPositive=()=>good/getAll()*100+"%"
-
 
   return (
     <div>
@@ -17,13 +28,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button> 
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>Statistics</h1>
-      <div> good: {good}</div>
-      <div>neutral: {neutral}</div>
-      <div>bad: {bad}</div>
-      <div>all: {getAll()}</div>
-      <div>average: {getAverage()}</div>
-      <div>positive: {getPositive()}</div>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
