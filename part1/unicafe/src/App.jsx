0024eeffaa22fc80506f4f022069205b1d3e9e0f
Pablo Benito <pablobenitomar@gmail.com>
2024-04-05 22:27:20 +0200

    exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 const StatisticLine = ({text,value}) => {
   return (
-    <div>{text}: {value}</div>
+    <tr><td>{text}</td><td>{value}</td></tr>
   )
 }
 
@@ -28,13 +28,14 @@ const Statistics = ({good,neutral,bad}) => {
     return (
       <div>
       <h1>Statistics</h1>
-      <StatisticLine text="good" value={good}/>
-      <StatisticLine text="neutral" value={neutral}/>
-      <StatisticLine text="bad" value={bad}/>
-      <StatisticLine text="bad" value={bad}/>
-      <StatisticLine text="all" value={good+neutral+bad}/>
-      <StatisticLine text="average" value={(good-bad)/(good+neutral+bad)}/>
-      <StatisticLine text="average" value={(good)/(good+neutral+bad)*100+" %"}/>
+      <table>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={good+neutral+bad}/>
+        <StatisticLine text="average" value={((good-bad)/(good+neutral+bad)).toFixed(2)}/>
+        <StatisticLine text="positive" value={((good)/(good+neutral+bad)*100).toFixed(1)+" %"}/>
+      </table>
 
       </div>
     )
